58639397bdce9a2eea66eb0c146cb3d3cfb90331
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-11-29 00:40:09 +0300

    EX003

--- EX003/Program.cs
+++ EX003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 56;
+﻿int numberA = new Random().Next(1, 10);
+Console.Write(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
